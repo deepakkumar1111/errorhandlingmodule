ab4cf587ca33dda64c65438c08d3ce5b054a28f0
DEEPAK KUMAR <dk7050110874@gmail.com>
2024-06-26 23:49:16 +0530

    Create errorhandling.sol

---
+++ errorhandling.sol
@@ -0,0 +1,27 @@
+This is code for errorhandling module for metacrafter
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+contract Deepak_error_handelling{
+
+    uint public count=0;
+
+    function Calories_Require(uint Calories) public{
+        require(Calories > 2000,"Calories  greater than 2000");
+        count++;
+    }
+
+    function Calories_Revert(uint Calories) public {
+        if(!(Calories > 2000)){
+            revert("Calories should be greater than 2000");
+        }
+        count++;
+    }
+
+    function Calories_Assert(uint Calories) public{
+        assert(Calories > 2000);
+        count++;
+    }
+    }
